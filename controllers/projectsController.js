9aab5ddfe69aed3bfb5e603fb6cd16a64d1fde82
Matias Aguilera <malbertoaguilera@outlook.es>
2021-10-12 15:57:46 -0300

    Creando el controlador de los proyectos

---
+++ controllers/projectsController.js
@@ -0,0 +1,7 @@
+exports.projectsHome = (request, response) => {
+  response.send("Index");
+};
+
+exports.projectsNosotros = (request, response) => {
+  response.send("Nosotros");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+//importar el controlador
+const projectsController = require('../controllers/projectsController');
+
 module.exports = function(){
 
       //ruta para el home
       //middleware de express
       //verbos post delete update get
       //'use' todos los verbos
-      router.get("/", (request, response) => {
-        response.send('Index');
-      });
+      router.get("/", projectsController.projectsHome);
       
-      router.get('/nosotros', (request, response) => {
-            response.send('Nosotros');
-      })
+      router.get('/nosotros', projectsController.projectsNosotros);
 
       return router;
 }
