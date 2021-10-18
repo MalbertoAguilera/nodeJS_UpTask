c889b619ef20e920b3f73a4e9a2bd430b7f15418
Matias Aguilera <malbertoaguilera@outlook.es>
2021-10-18 14:08:23 -0300

    Validacion simple controller/View

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,17 +1,36 @@
 exports.homeProject = (req, res) => {
   //render toma como parametro el nombre del html de pug
-  res.render("index",{
-    namePage : 'Proyectos'
+  res.render("index", {
+    namePage: "Proyectos",
   });
 };
 
 exports.formProject = (req, res) => {
-  res.render('newProject', {
-    namePage: 'Nuevo Proyecto'
+  res.render("newProject", {
+    namePage: "Nuevo Proyecto",
   });
-}
+};
 
 exports.newProject = (req, res) => {
   //Enviar a la consola
-  console.log(req.body);
-}
+  // console.log(req.body);
+
+  //validar el input
+  const { nombre } = req.body;
+  let errors = [];
+  console.log(nombre);
+
+  if (!nombre) {
+    errors.push({ text: "Agrega un nombre al proyecto" });
+  }
+
+  //si hay errores
+  if (errors.length) {
+    res.render("newProject", {
+      namePage: "NuevoProyecto",
+      errors
+    });
+  } else{
+    //insertar en una base de datos
+  }
+};
