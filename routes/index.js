c7aec566d99f52b4acf25268547c89f50ea70685
Matias Aguilera <malbertoaguilera@outlook.es>
2021-10-20 10:57:25 -0300

    Validar INPUT con express Validator

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+//importar express Validator
+const { body } = require("express-validator/check");
+
 //importar el controlador
-const projectsController = require('../controllers/projectsController');
+const projectsController = require("../controllers/projectsController");
 
-module.exports = function(){
-
-      //ruta para el home
-      //middleware de express
-      //verbos post delete update get
-      //'use' todos los verbos
-      router.get("/", projectsController.homeProject);
-      router.get('/new-project', projectsController.formProject);
-      router.post('/new-Project', projectsController.newProject);
-      return router;
-}
+module.exports = function () {
+  //ruta para el home
+  //middleware de express
+  //verbos post delete update get
+  //'use' todos los verbos
+  router.get("/", projectsController.homeProject);
+  router.get("/new-project", projectsController.formProject);
+  router.post("/new-Project",
+      body('name').not().isEmpty().trim().escape(),
+      projectsController.newProject);
+  return router;
+};
